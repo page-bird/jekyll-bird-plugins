216aa37f7e7c7f56c1b245f3f9d0a7aa8f7c9ff7
Alejo Rivera <alejo@juntostudio.com>
2020-03-24 18:27:52 -0600

    Misc improvements

--- lib/jekyll-bird-data-fetch/generator.rb
+++ lib/jekyll-bird-data-fetch/generator.rb
@@ -5,13 +5,15 @@ module JekyllBirdDataFetch
     safe true
 
     def generate(site)
-      jekyll_config = site.config['page_bird_data_fetch']
-      warn "No config for Page Bird Data Fetch".yellow && return if !jekyll_config
+      jekyll_config = site.config['page_bird']
+      warn "No config for Page Bird Data Fetch".yellow && return if !jekyll_config || !jekyll_config['website_id']
 
+      warn "Bird Fetching Data from: #{ Importer.endpoint(jekyll_config) }".green
       page_bird_data_fetch = Importer.call(jekyll_config)
       site.data["bird"] = page_bird_data_fetch
 
       @_file_creator ||= FileCreator.new(data: page_bird_data_fetch).call
+      warn "Bird fetched data successfully 🎺".green
     end
   end
 end
--- lib/jekyll-bird-data-fetch/importer.rb
+++ lib/jekyll-bird-data-fetch/importer.rb
@@ -3,14 +3,13 @@ require 'open-uri'
 
 module JekyllBirdDataFetch
   class Importer
-    def self.call(jekyll_config)
+    def self.call jekyll_config
       @_data ||= JSON.load(open(endpoint(jekyll_config)))
     end
 
-    private
-      def self.endpoint jekyll_config
-        host = jekyll_config["api_host"] || "https://www.page-bird.com"
-        host + "/api/w/" + jekyll_config["website"].to_s
-      end
+    def self.endpoint jekyll_config
+      host = jekyll_config["api_host"] || "https://www.page-bird.com"
+      host + "/api/w/" + jekyll_config["website_id"].to_s
+    end
   end
 end
